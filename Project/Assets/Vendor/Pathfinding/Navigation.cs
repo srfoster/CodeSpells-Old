8bce0516949458093b6e4f8830235250e44a0174
Jeff Compton <jscompton123@yahoo.com>
2012-05-29 17:18:55 -0700

    commit NavigationSystem

--- Project/Assets/Vendor/Pathfinding/Navigation.cs
+++ Project/Assets/Vendor/Pathfinding/Navigation.cs
@@ -1,148 +1,144 @@
 using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Navigation : MonoBehaviour {
-	/*
-	void OnGUI() {
-	    if (GUI.Button(new Rect(10,10,100,100),"Path find test"))
-		{
-			findPath(GameObject.Find("First Person Controller").transform.position, GameObject.Find("rock").transform.position);	
-		}
-	}
+	private List<Vector3> movements;
+	private Vertex endVertex = null;
+	private Vertex startVertex = null;
+	private bool has_started = false;
 	
-	public ArrayList findPath(Vector3 start, Vector3 end)
+
+	public List<Vector3> findPath(Vector3 start, Vector3 end)
 	{		
-		Vector3 waypoint_start = findClosest(start);
+		List<Vertex> allVertices = buildGraph(start, end);//start and end
 		
-		Vector3 waypoint_end   = findClosest(end);
+		List<Vertex> priorityVertices = new List<Vertex>();
+		Vertex currentVertex = null;
 		
-		ArrayList edges = getEdges();
-		
-		ArrayList edge1 = new ArrayList();
-		edge1.Add(start,end);
-		edges.add(edge1);
-		
-		ArrayList edge2 = new ArrayList();
-		edge2.Add(start,waypoint_start);
-		edges.add(edge2);
-		
-		ArrayList edge3 = new ArrayList();
-		edge3.Add(waypoint_end,end);
-		edges.add(edge3);
-		
-		ArrayList verts = getVerts();
-		verts.Add(start);
-		verts.Add(end);
-		
-		return shortestPath(edges, verts, start, end);
-	}
-	
-	public ArrayList shortestPath(ArrayList edges, ArrayList verts, Vector3 start, Vector3 end)
-	{
-		ArrayList explored = new ArrayList();
-		explored.Add(start);
-		
-		Hashtable distances = new Hashtable();
-		distances.Add(start,0);
-		
-		while(explored.Count < verts.Count)
-		{
+		startVertex.distTo = 0;
+		priorityVertices.Add (startVertex);
+		while (priorityVertices.Count != 0) { //pop off the element with the smallest priority
+			currentVertex = priorityVertices[0];
+			priorityVertices.RemoveAt(0);
 			
-			//Update distances
-			foreach(Vector3 v in verts)
-			{
-				foreach(Vector3 u in explored)
-				{
-					ArrayList edge = new ArrayList();
-					edge.Add(u,v);
-					
-					if(edges.Contains(edge)) //Probably won't work
-					{
-						if(distances[v] == null || Vector3.Distance(u,v) < distances[v])
-						{
-							distances[v] = Vector3.Distance(u,v) < distances[v];
-						}
+			foreach (Vertex vert in currentVertex.adj) { //get all adjacent vertices
+				if (!vert.hasTraversed) {					//distance along terrain I can make more advanced,
+															//In the future, I could precompute all the distances between
+															//adjacent waypoints and store them at the start or index them
+					if (vert.distTo > currentVertex.distTo + Vector3.Distance(currentVertex.myvector, vert.myvector)) {
+						vert.distTo = currentVertex.distTo + Vector3.Distance(currentVertex.myvector, vert.myvector);
+						vert.prev = currentVertex;
+						priorityVertices = enqueue(priorityVertices, vert); //add to queue
 					}
 				}
 			}
+			currentVertex.hasTraversed = true;
+		}
+		return finalPath (endVertex);
+	}
+	
+	private List<Vector3> finalPath(Vertex currentVertex) { 
+
+		List<Vertex> path = new List<Vertex>();
+		while(currentVertex.prev != null) {
+			path.Add (currentVertex);
+			currentVertex = currentVertex.prev;
+		}
+		path.Reverse ();
+		
+		List<Vector3> positions = new List<Vector3>();
+		foreach(Vertex vert in path) {
+			positions.Add (vert.myvector);
+		}
+		return positions;
+	}
+	
+	//adds the vertex to the right place in the priority queue
+	private List<Vertex> enqueue (List<Vertex> lv, Vertex vert) {
+		int counter = 0;
+		foreach (Vertex v in lv) {
+			if (v.distTo > vert.distTo) {
+				lv.Insert (counter, vert);
+				return lv;
+			}
+			counter++;
+		}
+		lv.Add(vert);
+		return lv;
+	}
+	
+	private List<Vertex> buildGraph(Vector3 start, Vector3 end) { //I can build the graph once in the future
+		
+		List<Vertex> allVertices = new List<Vertex>();
+		int counter = 0;
+		double connectToStartDist = Double.MaxValue;
+		double connectToEndDist = Double.MaxValue;
+		Vertex connectToStart = null;
+		Vertex connectToEnd = null;
+		
+		//transform = Get all the GameObjects from Unity that have Waypoint components
+		
+		//Debug.Log ("transform is null? :"+(transform == null));		
+		foreach (Transform t in transform) {
+			Waypoint w = t.gameObject.GetComponent<Waypoint>();
 			
-			//Pick closest
-			Vector3 closest = new Vector3();
-			
-			foreach(DictionaryEntry de in distances)
-			{
-				if(distances[closest] == null || distances[closest]  < de.Value)
-				{
-					closest = de.Key;	
+			Vertex temp = new Vertex(w.location());
+			allVertices.Add (temp);
+			if(Vector3.Distance(start, w.location ()) < connectToStartDist) {
+				connectToStartDist = Vector3.Distance(start, w.location ());
+				connectToStart = temp;
+			}
+			if(Vector3.Distance(end, w.location ()) < connectToEndDist) {
+				connectToEndDist = Vector3.Distance(end, w.location ());
+				connectToEnd = temp;
+			}
+		}
+		//allVertices has all vertices in the graph
+		foreach (Transform t in transform) { //for each waypoint
+			Waypoint w = t.gameObject.GetComponent<Waypoint>();
+
+			foreach (Vector3 tempAdj in w.getAdjacent()) { //for each adjacent Vector3
+				
+				foreach (Vertex vert in allVertices) {
+					if (vert.isSameLocation(tempAdj)) {
+						allVertices[counter].addAdjVert(vert);
+						break;
+					}
 				}
 			}
+			counter++;
 		}
-	}
-	
-	public Vector3 findClosest(Vector3 v)
-	{
-		Vector3 closest = new Vector3();
-		
+		startVertex = new Vertex(start);
+		endVertex = new Vertex(end);
+		startVertex.addAdjVert(connectToStart);
+		connectToEnd.addAdjVert(endVertex);
 
-		foreach(Transform child in transform)
-		{
-			Waypoint w = child.gameObject.GetComponent<Waypoint>();
-			
-			if(w == null)
-				continue;
-
-			if(Vector3.Distance(w.location(), v) < Vector3.Distance(closest,v))
-				closest = w.location();
-			
-			
-		}
+		allVertices.Add(startVertex);
+		allVertices.Add(endVertex);
 		
-		return closest;
+		return allVertices;
 	}
 	
-	public ArrayList getVerts()
-	{
-		ArrayList ret = new ArrayList();
+	private class Vertex {
+		public Vertex (Vector3 v) {
+			myvector = v;
+			hasTraversed = false;
+			distTo = Double.MaxValue;
+		}
+		public Vector3 myvector;
+		public double distTo;
+		public bool hasTraversed;
+		public List<Vertex> adj = new List<Vertex>();
+		public Vertex prev = null;
 		
-		foreach(Transform child in transform)
-		{
-			Waypoint w = child.gameObject.GetComponent<Waypoint>();
-			
-			if(w == null)
-				continue;
-			
-			ret.Add(w.location());
+		public void addAdjVert(Vertex v) {
+			adj.Add (v);	
 		}
 		
-		return ret;
-	}
-	
-	public ArrayList findWaypointPath()
-	{
-		ArrayList edges = new ArrayList();
-		
-		//Build the graph
-		foreach(Transform child in transform)
-		{
-			Waypoint w = child.gameObject.GetComponent<Waypoint>();
-			
-			if(w == null)
-				continue;
-			
-			foreach(Vector3 v in w.getAdjacent())
-			{
-				ArrayList edge1 = new ArrayList();
-				
-				edge1.Add(w.location());
-				edge1.Add(v);
-				
-				edges.Add(edge1);
-				
-			}
+		public bool isSameLocation(Vector3 v) {
+			return v == myvector;	
 		}
-		
-		
-		return edges;
 	}
-	*/
 }
