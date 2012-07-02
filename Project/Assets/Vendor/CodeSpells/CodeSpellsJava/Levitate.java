16006aec401f57d2bddb7f816e049e8c97f3f8a0
stephenfoster <srfoster@cs.ucsd.edu>
2012-07-02 14:33:36 -0700

    Merging Sarah's stuff

--- Project/Assets/Vendor/CodeSpells/CodeSpellsJava/Levitate.java
+++ Project/Assets/Vendor/CodeSpells/CodeSpellsJava/Levitate.java
@@ -3,26 +3,12 @@ import java.util.*;
 public class Levitate extends Spell
 {
   public void cast()
-  {
-     EnchantedList list = new EnchantedList();;
+  { 
+			Enchanted target = getTarget();
 
-   		for(int i = 1; i <=10; i++)
-     {
-			   Enchanted rock = getByName("Rock" + i);
-				  list.add(rock);
-     }
+    Location dest = target.getLocation();
+			dest.setY(dest.getY() + 10);
 
-     Enchanted last = list.get(0);
-
-     for(int i = 1; i < list.size(); i++)
-     {
-    		 Enchanted current = list.get(i);
-        Location dest = last.getLocation();
-        dest.setX(dest.getX() + 2f);
-						current.movement().teleport(dest);
-        last = current;  
-     }
-
-     list.movement().levitate(1f, 100f);
+    getByName("Player").movement().teleport(dest);    
   }
 }
