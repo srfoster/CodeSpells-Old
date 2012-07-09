a7a332a083d33b065fc10d91e7422e67fec2f13e
Jeff Compton <jscompton123@yahoo.com>
2012-07-09 15:35:34 -0700

    about to pull

--- Project/Assets/Vendor/Logger/FileLogger.cs
+++ Project/Assets/Vendor/Logger/FileLogger.cs
@@ -3,11 +3,11 @@ using System.Collections;
 using System.IO;
 
 public class FileLogger {
-	public static StreamWriter file = File.AppendText("/Users/stephenfoster/Desktop/CodeSpells.log");
+	public static StreamWriter file = File.AppendText("./CodeSpells.log");
 
-	
 	public static void Log(string message)		
 	{
+		
 		file.WriteLine("Unity: " + message);
 		file.Flush();
 	}
