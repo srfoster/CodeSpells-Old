21c5bdb4513f5149e7139c60c7cfd5e04089c103
Jeff Compton <jscompton123@yahoo.com>
2012-07-11 15:38:47 -0700

    about to pull

--- Project/Assets/Vendor/June/Java/src/Levitate.java
+++ Project/Assets/Vendor/June/Java/src/Levitate.java
@@ -5,30 +5,28 @@ public class Levitate extends Spell
 {
     public void cast()
     { 
-        System.out.println("INSIDE LEVITATE");
-        Enchanted destination = getByName("Destination");
-        Enchanted start = getByName("Rock1");
-        Enchanted center = getByName("Rock2");
+        //move rock to firepit
+        Enchanted 
         
-        System.out.println("about to get an enchantedlist");
-        EnchantedList list = center.findLike(center, 11.0);
-        Log.log("should have gotten enchantedlist");
         
+        
+        
+        /*Enchanted end = getByName("End");
+        Enchanted start = getByName("Start");
+        
+        EnchantedList list = getListByName("Rocks");
+        Direction direction = Direction.between(start,end);
 
-        Direction direction = Direction.between(start,destination);
-        Location tempLocation = start.getLocation();
-        int size = (list.size() < 11) ? list.size() : 10;
+        Location last = start.getLocation();
         
         
-        System.out.println("before for loop");
-        //make iterable next
-        for(int i = 0; i < size; i++)
+        for(Enchanted e : list)
         {
-            tempLocation.adjust(direction, 1.0);
-            //Log.log("new location is x: "+tempLocation.getX()+" y: "+tempLocation.getY()+" z: "+tempLocation.getZ());
-            list.get(i).setLocation(tempLocation);
-        }
-        System.out.println("after the loop");
+            last.add(direction);
+            e.setLocation(last);
+            last = e.getLocation();
+        }*/
+
     }
 }
 
