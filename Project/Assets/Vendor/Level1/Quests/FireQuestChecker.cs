f34f8f79850766f40622f5c5f372fbcd4b2a21fd
Sarah Esper <sesperucsd@gmail.com>
2012-07-12 14:12:55 -0700

    Making sure everything is in prefabs and moving the quests to Level1

---
+++ Project/Assets/Vendor/Level1/Quests/FireQuestChecker.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class FireQuestChecker : QuestChecker {
+	
+	public override bool checkIfCompleted()
+	{
+		GameObject crate1 = GameObject.Find("QuestFlammingCrate");
+		GameObject crate2 = GameObject.Find("QuestFlammingCrate1");
+		
+		if((crate1.GetComponent("Flamable") as Flamable).isIgnited() && (crate1.GetComponent("Flamable") as Flamable).isIgnited())
+			return true;
+		
+		return false;
+	}
+}
