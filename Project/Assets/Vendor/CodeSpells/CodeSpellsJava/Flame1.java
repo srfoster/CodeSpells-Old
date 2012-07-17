78a7e1365198f66d6ca285231e0a8cf95270154b
Sarah Esper <sesperucsd@gmail.com>
2012-07-17 12:50:22 -0700

    fixed gnomes a bit

--- Project/Assets/Vendor/CodeSpells/CodeSpellsJava/Flame1.java
+++ Project/Assets/Vendor/CodeSpells/CodeSpellsJava/Flame1.java
@@ -2,25 +2,8 @@ import june.*;
 
 public class Flame1 extends Spell{
   public void cast(){
-<<<<<<< HEAD
-    Enchanted thing = getTarget();
+    Enchanted thing = getByName("Rock1");
 
-    thing.onFire(true);
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-while(true) {
-        Enchanted obj = getTarget();
-        obj.scale();
-}
-=======
-    Enchanted target = getByName("Rock");
-=======
-    Enchanted target = getTarget();
->>>>>>> 7f4cafb7093ea620a09fdf20611ff91a67e2c69e
-
-    target.onFire(true);
->>>>>>> c8fbe9984f4909a59d0a7394d57a6a9e380b0332
->>>>>>> 7db0b824460af8cac0e8a55050c45e65995e52a7
+    thing.onFire(false);
   }
 }
