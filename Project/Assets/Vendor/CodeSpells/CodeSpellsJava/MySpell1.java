ba381ceebb66b4beaca4aa82b1b3451a26ffd2d7
stephenfoster <srfoster@cs.ucsd.edu>
2012-07-18 11:30:30 -0700

    Removed java path from complier messages.  Closes #159

--- Project/Assets/Vendor/CodeSpells/CodeSpellsJava/MySpell1.java
+++ Project/Assets/Vendor/CodeSpells/CodeSpellsJava/MySpell1.java
@@ -4,10 +4,8 @@ public class MySpell1 extends Spell{
   public void cast(){
     //Do magic here.
     
-    for(int i = 0; i < 10; i++){
-      //soething
-    }
+    Enchanted e = new Enchanted();
     
-     
+    Enchanted e = getByName();
   }
 }
