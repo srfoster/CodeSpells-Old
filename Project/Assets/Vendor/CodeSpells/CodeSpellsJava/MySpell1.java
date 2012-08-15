277173910ff8d85bd6c091daae71056de5ab76c4
overbey <overbey.e@gmail.com>
2012-08-15 14:26:29 -0700

    health decrements

--- Project/Assets/Vendor/CodeSpells/CodeSpellsJava/MySpell1.java
+++ Project/Assets/Vendor/CodeSpells/CodeSpellsJava/MySpell1.java
@@ -2,10 +2,7 @@ import june.*;
 
 public class MySpell1 extends Spell{
   public void cast(){
-    Enchanted area1 = getTarget();
-    area1.grow(40.0);
-    for (int i=0; i<30; i++) {
-      spawn("redCrate", area1.getLocation()).grow(2.0);
-      }
+    Location l = getTarget().getLocation();
+    spawn("redCrate", l);
   }
 }
---
+++ Project/Assets/Vendor/CodeSpells/DuelingModePrototype/Resources/PlayerCollider.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerCollider : MonoBehaviour {
+	
+	public System.Timers.Timer crateCollisionTimer;
+	public int timeInterval;
+	public double hitDamage;
+	
+	void OnTriggerEnter(Collider col) {
+		//start timed event
+		//if (col.gameObject().equals ())
+		Debug.Log ("Inside OnColliderEnter" + col.gameObject);
+		decreaseHealth(hitDamage, col);
+		//crateCollisionTimer = new System.Timers.Timer();
+		//crateCollisionTimer.Elapsed+=new ElapsedEventHandler(OnTimedEvent);
+	}
+	
+	
+	void OnCollisionExit(Collision col) {
+		//end timed event
+	}
+	
+	
+	void decreaseHealth(double amount, Collider col) {
+		if(col.gameObject.GetComponent<Health>() != null)
+			col.gameObject.GetComponent<Health>().decreaseHealth(amount);
+	}
+	
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
