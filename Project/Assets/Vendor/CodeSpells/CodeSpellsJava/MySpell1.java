e15b70d4ce166659f5d0cb3a3caa166dd486432b
Jeff Compton <jscompton123@yahoo.com>
2012-08-17 14:58:11 -0700

    about to push

--- Project/Assets/Vendor/CodeSpells/CodeSpellsJava/MySpell1.java
+++ Project/Assets/Vendor/CodeSpells/CodeSpellsJava/MySpell1.java
@@ -2,8 +2,8 @@ import june.*;
 
 public class MySpell1 extends Spell{
   public void cast(){
-    //Location tar = getByName("Area 1").getLocation();
-    //Enchanted newCrate = spawn("redCrate", tar);
-    getTarget().move(Direction.up(), 30.0f);
+    Enchanted tar = getTarget();
+    double val 
+    for (int i=0; i<0;
   }
 }
