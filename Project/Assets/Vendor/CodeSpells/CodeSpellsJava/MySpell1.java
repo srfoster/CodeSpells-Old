c809bd6edac2b60b1418f8774c27e547131b06a4
overbey <overbey.e@gmail.com>
2012-08-17 13:09:33 -0700

    players not enchantable

--- Project/Assets/Vendor/CodeSpells/CodeSpellsJava/MySpell1.java
+++ Project/Assets/Vendor/CodeSpells/CodeSpellsJava/MySpell1.java
@@ -3,6 +3,7 @@ import june.*;
 public class MySpell1 extends Spell{
   public void cast(){
     Location l = getTarget().getLocation();
-    spawn("redCrate", l);
+    for (int i = 0; i < 50; i++)
+      spawn("redCrate" , l);
   }
 }
