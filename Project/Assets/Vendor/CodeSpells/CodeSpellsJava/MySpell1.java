53b86120a50e4b7a1beb98dc02a3a4f4583d87ba
overbey <overbey.e@gmail.com>
2012-08-17 14:58:13 -0700

    so can pull

--- Project/Assets/Vendor/CodeSpells/CodeSpellsJava/MySpell1.java
+++ Project/Assets/Vendor/CodeSpells/CodeSpellsJava/MySpell1.java
@@ -2,8 +2,7 @@ import june.*;
 
 public class MySpell1 extends Spell{
   public void cast(){
-    //Location tar = getByName("Area 1").getLocation();
-    //Enchanted newCrate = spawn("redCrate", tar);
-    getTarget().move(Direction.up(), 30.0f);
+    Location l = getTarget().getLocation();
+    spawn("redCrate", l);
   }
 }
