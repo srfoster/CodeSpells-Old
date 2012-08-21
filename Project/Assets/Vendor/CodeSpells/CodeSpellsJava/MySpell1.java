dbf8cb09cdb61e6edea6b7b382382f6111f31e85
overbey <overbey.e@gmail.com>
2012-08-21 13:10:21 -0700

    in order to pull

---
+++ Project/Assets/Vendor/CodeSpells/CodeSpellsJava/MySpell1.java
@@ -0,0 +1,9 @@
+import june.*;
+
+public class MySpell1 extends Spell{
+  public void cast(){
+    Location l = getTarget().getLocation();
+    Enchanted e = spawn("Red
+    e.grow();
+  }
+}
--- Project/Assets/Vendor/CodeSpells/CodeSpellsJava/MySpell2.java
+++ Project/Assets/Vendor/CodeSpells/CodeSpellsJava/MySpell2.java
@@ -1,9 +1,8 @@
 import june.*;
 
-public class Fly extends Spell{
+public class MySpell2 extends Spell{
   public void cast(){
-    Location l = getTarget().getLocation();
-    for(int i=0; i < 10; i++)
-      spawn("redCrate", l);
+    Enchanted s = getTarget();
+    s.move(Direction.forward());
   }
 }
