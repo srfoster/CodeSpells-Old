70d73a6552a4c3e0fa210a4bb002cc9ff32c5c1b
Jeff Compton <jscompton123@yahoo.com>
2012-08-28 13:39:23 -0700

    about to commit

--- Project/Assets/Vendor/CodeSpells/CodeSpellsJava/MySpell1.java
+++ Project/Assets/Vendor/CodeSpells/CodeSpellsJava/MySpell1.java
@@ -1,8 +1,28 @@
 import june.*;
-
-public class MySpell1 extends Spell{
+ 
+public class CrateRain extends Spell{
   public void cast(){
-    Enchanted en = spawn("redCrateNG", getTarget().getLocation());
-    en.move(Direction.up(), 7.0);
+    EnchantedList all = new EnchantedList();
+    Location loc = getTarget().getLocation();
+    Enchanted me = getByName("Me");
+ 
+    for(int i=0; i < 30; i++)
+    {
+      Enchanted e = spawn("redCrate", loc);
+      all.add(e);
+    }
+ 
+    while(true)
+    {
+      Direction dir = Direction.forward();
+ 
+      Vector3 dest = me.getLocation();
+      dest = dest.add(dir.times(20));
+ 
+      for(Enchanted e : all)
+      {
+        e.setLocation((Location)dest);
+      }
+    }
   }
 }
--- Project/Assets/Vendor/CodeSpells/CodeSpellsJava/MySpell2.java
+++ Project/Assets/Vendor/CodeSpells/CodeSpellsJava/MySpell2.java
@@ -1,31 +1,8 @@
 import june.*;
 
-public class Shield extends Spell{
+public class MySpell2 extends Spell{
   public void cast(){
-    Enchanted myCrate1 = spawn("redCrate",
-      getTarget().getLocation());
-        Enchanted myCrate2 = spawn("redCrate",
-      getTarget().getLocation());
-        Enchanted myCrate3 = spawn("redCrate",
-      getTarget().getLocation());
-        Enchanted myCrate4 = spawn("redCrate",
-      getTarget().getLocation());
-        Enchanted myCrate5 = spawn("redCrate",
-      getTarget().getLocation());
+    Enchanted en = spawn("redCrateNG", getTarget().getLocation());
     
-    Location myLoc = getByName("Me").getLocation();
-    
-    while(true){
-        myCrate1.setLocation(myLoc);
-        myCrate1.move(Direction.up(), 3);
-              myCrate2.setLocation(myLoc);
-        myCrate2.move(Direction.up(), 3);
-              myCrate3.setLocation(myLoc);
-        myCrate3.move(Direction.up(), 3);
-              myCrate4.setLocation(myLoc);
-        myCrate4.move(Direction.up(), 3);
-              myCrate5.setLocation(myLoc);
-        myCrate5.move(Direction.up(), 3);
-      }
   }
 }
