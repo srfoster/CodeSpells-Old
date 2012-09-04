d8dbca097ca9aa1cf6efc15298f62886ebcd6c73
overbey <overbey.e@gmail.com>
2012-09-04 12:35:44 -0700

    to push

--- Project/Assets/Vendor/CodeSpells/DuelingModePrototype/Resources/PlayerCollider.cs
+++ Project/Assets/Vendor/CodeSpells/DuelingModePrototype/Resources/PlayerCollider.cs
@@ -33,17 +33,13 @@ public class PlayerCollider : MonoBehaviour {
 	}
 	
 	
-<<<<<<< HEAD
-	void decreaseHealth(double amount, Collider col) {
-		if(col.gameObject.GetComponent<Health>() != null && col.gameObject.GetComponent<ExampleDisplayHealth>().heart_texture.name == heartColor)
 
-			col.gameObject.GetComponent<Health>().decreaseHealth(10);
 
-=======
+
 	void decreaseHealth(double amount, GameObject gameObject) {
 		if(gameObject.GetComponent<Health>() != null && gameObject.GetComponent<ExampleDisplayHealth>().heart_texture.name == heartColor)
 			gameObject.GetComponent<Health>().decreaseHealth(10.0);
->>>>>>> 36fb2109e75ee725ae5ca250c4235c32b501a9b6
+
 	}
 	
 	
