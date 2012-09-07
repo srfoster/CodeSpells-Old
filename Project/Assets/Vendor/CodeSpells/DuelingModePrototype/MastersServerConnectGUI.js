187442cf06236a929fa208a000cd4646bf5794a7
overbey <overbey.e@gmail.com>
2012-09-07 13:25:13 -0700

    to push

--- Project/Assets/Vendor/CodeSpells/DuelingModePrototype/MastersServerConnectGUI.js
+++ Project/Assets/Vendor/CodeSpells/DuelingModePrototype/MastersServerConnectGUI.js
@@ -245,9 +245,7 @@ function OnServerInitialized()
 	GameObject.Find("SpawnServer").GetComponent("SpawnPlayer").Spawn();
 	
 			
-	var crate_gui_prefab = Resources.Load("CrateGUI");
-		
-	Network.Instantiate(crate_gui_prefab, transform.position, Quaternion.identity, 0);
+
 
 }
 
