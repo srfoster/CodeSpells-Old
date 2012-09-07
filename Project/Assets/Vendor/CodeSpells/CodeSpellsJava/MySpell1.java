693528399e6053dc02be55ac2e28043e4234f989
overbey <overbey.e@gmail.com>
2012-09-07 12:42:19 -0700

    to pull

--- Project/Assets/Vendor/CodeSpells/CodeSpellsJava/MySpell1.java
+++ Project/Assets/Vendor/CodeSpells/CodeSpellsJava/MySpell1.java
@@ -1,23 +1,91 @@
 import june.*;
- 
-public class TrickyShoot extends Spell{
+
+public class MySpell1 extends Spell{
   public void cast(){
-    Enchanted me = getByName("Me");
+    Enchanted c1 = makeBox();
  
-    Enchanted c1 = spawn("redCrate", me.getLocation());
+     while(true)
+     {
  
-    double i = 0;
-    while(true){
-      double x = Math.sin(i)*20;
+      boxInFront(c1);
  
-      Direction dir = Direction.forward();
  
-      Vector3 dest = me.getLocation();
-      dest = dest.add(dir.times(25+x));
+      if(isJumping())
+        //popup("Jumping!");
+        spawnBoxes(c1);
+      
  
-      c1.setLocation((Location)dest);
- 
-      i++;
     }
   }
+ 
+ 
+  public boolean isJumping()
+  {
+      LazyLocation l = (LazyLocation) getByName("Me").getLocation();
+      l.freeze();
+ 
+ 
+      String s = l.getYString();
+ 
+      String[] split_s = s.split(",");
+      String y_string = split_s[1];
+      double y = Double.parseDouble(y_string);
+ 
+      return y > 3.0;
+ 
+  }
+ 
+  public void popup(String s)
+  {
+      (new Enchanted("")).executeCommand("util.popup('"+s+"')");
+  }
+ 
+  public void boxInFront(Enchanted c1)
+  {
+ 
+    Direction dir = Direction.forward();
+    Vector3 dest = getByName("Me").getLocation();
+    dest = dest.add(dir.times(30));
+    c1.setLocation((Location)dest);
+ 
+  }
+ 
+  public Enchanted makeBox()
+  {
+ 
+    Enchanted e = getByName("Area 1");
+    Enchanted me = getByName("Me");
+    Enchanted c1 = spawn("redCrateNG", e.getLocation());
+ 
+    return c1;
+  }
+  
+  public void spawnBoxes(Enchanted c1)
+  {
+    EnchantedList list = new EnchantedList();
+ 
+    for(int i=0; i < 5; i++)
+    {
+      for(int j = 0; j < 5; j++)
+      {
+        Location l = mod(i*2.5,0,j*2.5, c1.getLocation());
+ 
+        Enchanted e = spawn("redCrateNG", l);
+ 
+        list.add(e);
+      }
+    }
+  }
+  
+  public Location mod(double x, double y, double z, Location loc)
+  {
+    Location ret = loc;
+ 
+    ret = (Location) ret.add(Direction.forward().times(z));
+    ret = (Location) ret.add(Direction.right().times(x));
+    ret = (Location) ret.add(Direction.up().times(y));
+ 
+    return ret;
+   }
+  
 }
--- Project/Assets/Vendor/CodeSpells/CodeSpellsJava/MySpell2.java
+++ Project/Assets/Vendor/CodeSpells/CodeSpellsJava/MySpell2.java
@@ -1,54 +1,12 @@
 import june.*;
- 
-public class TrickyShoot extends Spell{
+
+public class MySpell2 extends Spell{
   public void cast(){
-<<<<<<< HEAD
-     Enchanted my_target = getByName("myCrate");
-    Enchanted me = getByName("Me");
-    EnchantedList all = new EnchantedList();
-
-
-    for(int i=0; i<29; i++) {
-      all.add(spawn("redCrate", my_target.getLocation()));
-      }
-
-    int counter = 0;
-    while(counter < 30)
-    {
-      my_target.move(Direction.up(), 0.1);
-      counter = counter + 1;
-    }
-        int count=0;
-    while(true && my_target.distanceTo(me) < 10){
-        count++;
-       my_target.move(Direction.forward(), 0.2);
-      if (count%20==0) {
-        for(Enchanted e:all) {
-        e.setLocation(my_target.getLocation());
-          }
-        }
-    }
-=======
-    Enchanted me = getByName("Me");
- 
-    Enchanted c1 = spawn("redCrate", me.getLocation());
- 
-    double i = 0;
-    while(true){
-      double x = Math.sin(i)*20;
- 
-      Direction dir = Direction.forward();
- 
-      Vector3 dest = me.getLocation();
-      dest = dest.add(dir.times(25+x));
- 
-      c1.setLocation((Location)dest);
- 
-      i++;
-    }
+Enchanted myCrate = spawn("redCrate", getTarget().getLocation());
+Location myLoc = getByName("Me").getLocation();
+while(true) {
+myCrate.setLocation(myLoc);
+myCrate.move(Direction.forward(), 10);
   }
 }
-<<<<<<< HEAD
->>>>>>> 422fb8412fe4776f4955a0e7c59cf5481a933be5
-=======
->>>>>>> 36fb2109e75ee725ae5ca250c4235c32b501a9b6
+ } 
