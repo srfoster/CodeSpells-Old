145bf3c016b0cdc59af3ca01383339ce125ae99b
Sarah Esper <sesperucsd@gmail.com>
2013-02-14 17:41:43 -0800

    Fixed the error where the camera would end up too low after the monster attacked

--- Project/Assets/Standard Assets/CodeSpells/CharacterControllers/Sources/Scripts/ShakeCamera.cs
+++ Project/Assets/Standard Assets/CodeSpells/CharacterControllers/Sources/Scripts/ShakeCamera.cs
@@ -5,27 +5,21 @@ public class ShakeCamera : MonoBehaviour {
 
 	private bool shakeMode = false;
 	private bool up = true;
-	private Vector3 myPos;
+	
 	// Use this for initialization
 	public void startShakeMode() {
-		Debug.Log("set shake mode to true at position: "+transform.position);
-		myPos = transform.position;
-		Debug.Log("at starting shake mode, mypos = "+myPos);
+		//Start Shaking
 		shakeMode = true;
 	}
 	
 	public void endShakeMode() {
-		//resets position
-		shakeMode = false; 
-		Debug.Log("returning to start position: "+myPos+" from position: "+transform.position);
-		transform.position = new Vector3(transform.position.x, myPos.y, transform.position.z);
+		//Stop Shaking
+		shakeMode = false;
 	}
 	
 	// Update is called once per frame
 	public void UpdateShake() {
-		Debug.Log("In update and shakeMode is: "+shakeMode);
-		if (shakeMode) { //
-			Debug.Log("shaking");
+		if (shakeMode) {
 			transform.position = new Vector3(transform.position.x, transform.position.y + (up ? 0.07f : -0.07f), transform.position.z);
 			up = !up;
 		}
