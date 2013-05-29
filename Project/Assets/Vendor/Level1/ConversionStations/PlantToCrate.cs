2696746848c276d90c0d0e0892857651a500d337
Sarah Esper <sesperucsd@gmail.com>
2013-05-29 00:00:32 -0700

    added two pages to the spellbook

--- Project/Assets/Vendor/Level1/ConversionStations/PlantToCrate.cs
+++ Project/Assets/Vendor/Level1/ConversionStations/PlantToCrate.cs
@@ -10,25 +10,25 @@ public class PlantToCrate : ConvertOnEntry {
 
 		if(plant.GetComponent("Substance") == null)
 		{
-			Debug.Log("Was not a substance");
+			//Debug.Log("Was not a substance");
 			return false;
 		}
 		
 		if(!(plant.GetComponent("Substance") as Substance).isPlant())
 		{
-			Debug.Log("Was not a plant");
+			//Debug.Log("Was not a plant");
 			return false;
 		}
 		
 		if(plant.GetComponent("Growable") == null)
 		{
-			Debug.Log("Was not growable");
+			//Debug.Log("Was not growable");
 			return false;
 		}
 
 		if(!(plant.GetComponent("Growable") as Waterable).isWaterlogged())
 		{
-			Debug.Log("Was not waterlogged");
+			//Debug.Log("Was not waterlogged");
 			return false;
 		}
 		
